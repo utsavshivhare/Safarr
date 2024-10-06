d33a566d3e14e80de708463f616801f7dc045aa1
ushivhare <utsavshivhare@gmail.com>
2024-10-06 19:49:48 +0530

    basic

---
+++ src/main/java/com/safarr/app/SafarrApplication.java
@@ -0,0 +1,13 @@
+package com.safarr.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafarrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafarrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/safarr/app/SafarrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safarr.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafarrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
